24b18dda2acead38a473ad7f90122e33c6428f62
sush3011 <65852822+sush3011@users.noreply.github.com>
2020-08-24 19:07:20 +0100

    Add files via upload

---
+++ Project/img/spritesheet_jumper.py
@@ -0,0 +1,101 @@
+# bunnyBrown animations (x, y, width, height)
+bunnyBrown_hurt = (382, 946, 150, 174)
+bunnyBrown_jump = (382, 763, 150, 181)
+bunnyBrown_idle1 = (614, 1063, 120, 191)
+bunnyBrown_idle2 = (690, 406, 120, 201)
+bunnyBrown_walk1 = (678, 860, 120, 201)
+bunnyBrown_walk2 = (692, 1458, 120, 207)
+
+# bunnyPurple animations (x, y, width, height)
+bunnyPurple_hurt = (411, 1866, 150, 174)
+bunnyPurple_jump = (416, 1660, 150, 181)
+bunnyPurple_idle1 = (581, 1265, 121, 191)
+bunnyPurple_idle2 = (584, 0, 121, 201)
+bunnyPurple_walk1 = (584, 203, 121, 201)
+bunnyPurple_walk2 = (678, 651, 121, 207)
+
+# Platforms
+platformGrass = (0, 288, 380, 94)
+platformGrass_small = (213, 1662, 201, 100)
+
+platformCake = (0, 576, 380, 94)
+platformCake_small = (218, 1456, 201, 100)
+
+platformSand = (0, 672, 380, 94)
+platformSand_small = (208, 1879, 201, 100)
+
+platformSnow = (0, 768, 380, 94)
+platformSnow_small = (213, 1764, 201, 100)
+
+platformStone = (0, 96, 380, 94)
+platformStone_small = (382, 408, 200, 100)
+
+platformWood = (0, 960, 380, 94)
+platformWood_small = (218, 1558, 200, 100)
+
+# Broken Platforms
+platformGrass_broken = (0, 384, 380, 94)
+platformGrass_broken_small = (382, 204, 200, 100)
+
+platformCake_broken = (0, 0, 380, 94)
+platformCake_broken_small = (262, 1152, 200, 100)
+
+platformSand_broken = (0, 1056, 380, 94)
+platformSand_broken_small = (382, 102, 200, 100)
+
+platformSnow_broken = (0, 480, 380, 94)
+platformSnow_broken_small = (382, 306, 200, 100)
+
+platformStone_broken = (0, 192, 380, 94)
+platformStone_broken_small = (232, 1288, 200, 100)
+
+platformWood_broken = (0, 864, 380, 94)
+platformWood_broken_small = (382, 0, 200, 100)
+
+# Powerups
+powerup_bubble = (826, 134, 71, 70)
+powerup_jumpBoost = (826, 1220, 71, 70)
+powerup_empty = (894, 1661, 71, 70)
+powerup_jetpack = (820, 1805, 71, 70)
+powerup_wings = (826, 1292, 71, 70)
+
+# Collectables
+carrot = (820, 1733, 78, 70)
+
+# Enemies
+spikeBall_1 = (534, 763, 142, 148)
+spikeBall_2 = (464, 1122, 148, 141)
+
+spikeMan_jump = (736, 1063, 114, 155)
+spikeMan_stand = (814, 1417, 90, 155)
+spikeMan_walk1 = (704, 1256, 120, 159)
+spikeMan_walk2 = (812, 296, 90, 155)
+
+springMan_stand = (801, 609, 110, 141)
+springMan_hurt = (800, 860, 110, 141)
+
+wingMan1 = (382, 635, 174, 126)
+wingMan2 = (0, 1879, 206, 107)
+wingMan3 = (0, 1559, 216, 101)
+wingMan4 = (0, 1456, 216, 101)
+wingMan5 = (382, 510, 182, 123)
+
+flyMan_up = (566, 510, 122, 139)
+flyMan_down = (568, 1534, 122, 135)
+flyMan_hover = (568, 1671, 122, 139)
+flyMan_fall = (692, 1667, 120, 132)
+flyMan_jump = (698, 1801, 120, 128)
+flyMan_stand = (707, 0, 120, 132)
+
+# obstacles
+lighting_blue = (895, 453, 55, 114)
+lighting_yellow = (897, 0, 55, 114)
+
+spike_bottom = (894, 206, 51, 87)
+spike_top = (885, 752, 51, 87)
+
+spikes_bottom = (147, 1988, 95, 53)
+spikes_top = (232, 1390, 95, 53)
+
+
+carrots_collected = (812, 554, 54, 49)
